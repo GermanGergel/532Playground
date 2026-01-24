406d4155ab1dcce2bc28392722cfab9a2e9bfaac
GermanGergel <polskyiyan@gmail.com>
2026-01-24 17:43:14 +0700

    feat: Use BrowserRouter for routing
    Switches the application's client-side routing from HashRouter to BrowserRouter. This enables cleaner URLs and improves SEO by removing the hash fragment from the URL.

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AppProvider } from './context';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -13,10 +13,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
